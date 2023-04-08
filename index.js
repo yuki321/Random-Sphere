eab2a376fd9089c0b48e2ea72716fb138e034642
yuki321 <yukiikuy@live.jp>
2023-04-09 01:17:26 +0900

    ブラウザ座標を3D座標に変換する処理を追加

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 'use strict';
 
 window.addEventListener('DOMContentLoaded', init);
+const WIDTH = 1000;
+const HEIGHT = 1000;
 
 function init(){
-    const WIDTH = 1000;
-    const HEIGHT = 1000;
 
     const renderer = new THREE.WebGLRenderer({
         canvas: document.getElementById('canvas'),
@@ -27,7 +27,6 @@ function init(){
             // Geometry 半径,緯度分割数,経度分割数
             const geometry = new THREE.SphereGeometry(20, 40, 20);
             // Material
-            // const material = new THREE.MeshNormalMaterial();
             let color = "0x" + Math.floor(Math.random() * 0xffffff).toString();
             const material = new THREE.MeshBasicMaterial({color: Number(color)});
             // Mesh
@@ -39,13 +38,40 @@ function init(){
     animate();
 
     function animate(){
+        getPos();
         renderer.render(scene, camera);
         requestAnimationFrame(animate);
-
     }
-
 }
 
+function getPos(){
+    document.addEventListener("mousemove", function(e){
+        let posX = convertPosX(e.clientX);
+        let posY = convertPosY(e.clientY);
+        // For Debug
+        console.log("X: " + posX, "clientX: " + e.clientX, "Y: " + posY, "clientY: " + e.clientY);
+    });
+}
 
+/**
+ * convert browser coordinates to 3D coordinates
+ * @param pos
+ * @returns convertedPos
+ */
+function convertPosX(pos){
+    let convertedPos = pos - WIDTH / 2;
+    return convertedPos;
+}
+function convertPosY(pos){
+    let convertedPos = pos - HEIGHT / 2;
+    if(pos < 500){
+        convertedPos = -(convertedPos);
+    }else if(pos > 500){
+        // NP
+    }else{
+        convertedPos = 0;
+    }
+    return convertedPos;
+}
 
 
