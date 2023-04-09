2b0a7356f05e7c6416faaa74806c13cf25bdf773
yuki321 <yukiikuy@live.jp>
2023-04-09 10:55:02 +0900

    座標変換処理削除。Mesh作成処理をanimate関数に移動

--- index.js
+++ index.js
@@ -14,64 +14,36 @@ function init(){
 
     // Camera
     const camera = new THREE.PerspectiveCamera(45, WIDTH / HEIGHT, 1, 1000);
-    // camera.position.set(-100, 150, 500);
     camera.position.set(0, 0, 1000);
 
-    // Scene
-    const scene = new THREE.Scene();
-    const NUM_OF_WIDTH = 10;
-    const NUM_OF_HEIGHT = 10;
-
-    for(let i = 0; i < NUM_OF_HEIGHT; i++) {
-        for(let j = 0; j < NUM_OF_WIDTH; j++) {
-            // Geometry 半径,緯度分割数,経度分割数
-            const geometry = new THREE.SphereGeometry(20, 40, 20);
-            // Material
-            let color = "0x" + Math.floor(Math.random() * 0xffffff).toString();
-            const material = new THREE.MeshBasicMaterial({color: Number(color)});
-            // Mesh
-            const mesh = new THREE.Mesh(geometry, material);
-            mesh.position.set(i*50-250, j*50-250);
-            scene.add(mesh);
-        }
-    }
     animate();
 
     function animate(){
-        getPos();
+        // Scene
+        const scene = new THREE.Scene();
+        const NUM_OF_WIDTH = 10;
+        const NUM_OF_HEIGHT = 10;
+
+        for(let i = 0; i < NUM_OF_HEIGHT; i++) {
+            for(let j = 0; j < NUM_OF_WIDTH; j++) {
+                // Geometry
+                const geometry = new THREE.SphereGeometry(20, 40, 20);
+                // Material
+                let color = "0x" + Math.floor(Math.random() * 0xffffff).toString();
+                const material = new THREE.MeshBasicMaterial({color: Number(color)});
+                // Mesh
+                const mesh = new THREE.Mesh(geometry, material);
+                mesh.position.set(i*50-250, j*50-250);
+                scene.add(mesh);
+            }
+        }
+
         renderer.render(scene, camera);
         requestAnimationFrame(animate);
     }
+
 }
 
-function getPos(){
-    document.addEventListener("mousemove", function(e){
-        let posX = convertPosX(e.clientX);
-        let posY = convertPosY(e.clientY);
-        // For Debug
-        console.log("X: " + posX, "clientX: " + e.clientX, "Y: " + posY, "clientY: " + e.clientY);
-    });
-}
 
-/**
- * convert browser coordinates to 3D coordinates
- * @param pos
- * @returns convertedPos
- */
-function convertPosX(pos){
-    let convertedPos = pos - WIDTH / 2;
-    return convertedPos;
-}
-function convertPosY(pos){
-    let convertedPos = pos - HEIGHT / 2;
-    if(pos < 500){
-        convertedPos = -(convertedPos);
-    }else if(pos > 500){
-        // NP
-    }else{
-        convertedPos = 0;
-    }
-    return convertedPos;
-}
 
 
